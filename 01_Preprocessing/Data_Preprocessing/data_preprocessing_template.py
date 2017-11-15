4a91a483a1e83df3f2ebf1cfc83a944fff527b87
christon88 <c.a.onsager@gmail.com>
2017-11-15 21:12:02 +0100

    Added some preprocessing

--- 01_Preprocessing/Data_Preprocessing/data_preprocessing_template.py
+++ 01_Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -7,12 +7,21 @@ import pandas as pd
 
 #Importing the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, 3].values
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
 
 #Fix missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-print(X)
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+#Categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+le_x = LabelEncoder()
+x[:, 0] = le_x.fit_transform(x[:, 0])
+ohe = OneHotEncoder(categorical_features = [0])
+x = ohe.fit_transform(x).toarray()
+
+le_y = LabelEncoder()
+y = le_y.fit_transform(y)
