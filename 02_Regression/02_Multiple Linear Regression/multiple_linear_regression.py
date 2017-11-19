a3442adbe05db65867b480aa9ad5ee1f24521406
christon88 <c.a.onsager@gmail.com>
2017-11-19 10:23:06 +0100

    More work on multiple regression

--- 02_Regression/02_Multiple Linear Regression/multiple_linear_regression.py
+++ 02_Regression/02_Multiple Linear Regression/multiple_linear_regression.py
@@ -14,12 +14,12 @@ y = dataset.iloc[:, no_vars].values
 #Categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 le_x = LabelEncoder()
-x[:, 3] = le_x.fit_transform(x[:, 3])
-ohe = OneHotEncoder(categorical_features = [3])
+x[:, 3] = le_x.fit_transform(x[:, 3]) #Changes text to numbers
+ohe = OneHotEncoder(categorical_features = [3]) #Dummy codes this column
 x = ohe.fit_transform(x).toarray()
 
-le_y = LabelEncoder()
-y = le_y.fit_transform(y)
+#Removing first dummy variable
+x = x[:, 1:]
 
 # Feature scaling
 """from sklearn.preprocessing import StandardScaler
@@ -29,3 +29,14 @@ x[:, 3:] = sc_x.fit_transform(x[:, 1:2])"""
 # Splitting data into taining and test set
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+#Fitting multiple linear regression to training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+y_pred = regressor.predict(x_test)
+
+#Backward Elimination
+import statsmodels.formula.api as sm
+x = np.append(arr = np.ones((50, 1)).astype(int), values = x, axis = 1)
